ef78d430f152ba8b67b94a982e45448c9569559b
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-04 01:04:49 +0200

    делаю 3 задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const lastItem = document.createElement("li");
+  lastItem.classList.add("item");
+  lastItem.textContent = ingredient;
+  list.append(lastItem);
+}
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+
+for (const image of images) {
+  const listItemImage = `<li class="item"><img class="img" src="${image.url}" alt="${image.alt}"></li>`;
+  gallery.insertAdjacentHTML("beforeend", listItemImage);
+}
