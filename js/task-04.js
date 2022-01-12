20e6d728e85d6fb186cc607a1e6fec2cfdf7fb16
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-12 17:27:33 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resultButtons = document.querySelector("#value");
+
+
+let counterValue = 0;
+
+decrementBtn.addEventListener("click", () => {
+    counterValue += 1;
+    console.log(counterValue);
+});
+
+
+
+
