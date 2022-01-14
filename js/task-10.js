4959f4e5144e7bb38481a7521b332c9a59251eea
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-14 23:14:09 +0200

    дз 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,26 +3,23 @@ function getRandomHexColor() {
 }
 
 
-const inputAmountElements = document.querySelector('input[type="number"]');
+const input = document.querySelector('[type="number"]');
 const сreateBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxes = document.querySelector("#boxes");
 
 
 сreateBtn.addEventListener("click", () => {
-  const length = inputAmountElements.value
+  const length = input.value;
   
   const result = [];
   
   for (let i = 0; i <= length - 1; i++) {
     const blockElement = document.createElement("div");
     const multiplicationСycle = 30 + i * 10;
-
-    blockElement.classList.add("boxes_item");
     blockElement.style.backgroundColor = getRandomHexColor();
     blockElement.style.width = `${multiplicationСycle}px`;
     blockElement.style.height = `${multiplicationСycle}px`;
-
     result.push(blockElement);
   }
 
@@ -36,7 +33,6 @@ destroyBtn.addEventListener("click", () => {
   const items = [];
 
   for (const listItem of listItems) {
-    console.log(listItem);
     items.push(listItem);
   }
 
