cfcda6bdae154b9e002dcec6f827db49b09e03da
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-15 20:28:58 +0200

    меняю на тернарник

--- js/task-05.js
+++ js/task-05.js
@@ -5,10 +5,15 @@ const withoutInput = output.textContent;
 
 textInput.addEventListener("input", (event) => {
 
-    if (!event.currentTarget.value) {
-        output.textContent = withoutInput;
-    } else {
-        output.textContent = event.currentTarget.value;
-    };
+    // //V2
+    // if (!event.currentTarget.value) {
+    //     output.textContent = withoutInput;
+    // } else {
+    //     output.textContent = event.currentTarget.value;
+    // };
+
+    !event.currentTarget.value
+        ? output.textContent = withoutInput
+        : output.textContent = event.currentTarget.value;
     
 });
