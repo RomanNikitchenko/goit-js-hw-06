c4a1b28e7756b87d6ebcca4269a68ae9da618330
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-10-29 00:38:29 +0300

    new СounterPlugin

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,56 @@
+// функция-конструктор
 
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
-const resultButtons = document.querySelector("#value");
+const СounterPlugin = function ({ rootSelector, initialValue = 0, step = 1 } = {}) {
+  this._value = initialValue;
+  this._step = step;
+  this._refs = this._getRefs(rootSelector);
+  this._bindEvents();
+  this.updateResultUI();
+};
 
-let counterValue = 0;
+СounterPlugin.prototype._getRefs = function (rootSelector) {
+  const refs = {};
 
-decrementBtn.addEventListener("click", () => {
-    counterValue -= 1;
-    resultButtons.textContent = counterValue;
+  refs.container = document.querySelector(rootSelector);
+  refs.incrementBtn = refs.container.querySelector('[data-action="increment"]');
+  refs.decrementBtn = refs.container.querySelector('[data-action="decrement"]');
+  refs.resultButtons = refs.container.querySelector('#value');
+
+  return refs;
+};
+
+СounterPlugin.prototype.increment = function () {
+  this._value += this._step;
+};
+
+СounterPlugin.prototype.decrement = function () {
+  this._value -= this._step;
+};
+
+СounterPlugin.prototype.updateResultUI = function () {
+  this._refs.resultButtons.textContent = this._value;
+};
+
+СounterPlugin.prototype._bindEvents = function () {
+  this._refs.incrementBtn.addEventListener('click', () => {
+    this.increment();
+    this.updateResultUI();
+  });
+
+  this._refs.decrementBtn.addEventListener('click', () => {
+    this.decrement();
+    this.updateResultUI();
+  });
+};
+
+new СounterPlugin({
+  rootSelector: '#counter-1',
+  initialValue: 100,
+  step: 1,
 });
 
-incrementBtn.addEventListener("click", () => {
-    counterValue += 1;
-    resultButtons.textContent = counterValue;
+new СounterPlugin({
+  rootSelector: '#counter-2',
+  initialValue: 0,
+  step: 2,
 });
